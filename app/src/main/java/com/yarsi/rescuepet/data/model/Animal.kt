88a61de3b2cd75afb70c1e0c8cb228d6d40c85da
yandevxxx <zildjian1121@gmail.com>
2026-06-07 19:08:26 +0700

    Constanta Appwrite, Result, Data Animal

---
+++ app/src/main/java/com/yarsi/rescuepet/data/model/Animal.kt
@@ -0,0 +1,16 @@
+package com.yarsi.rescuepet.data.model
+
+data class Animal(
+    val id: String = "",
+    val type: String = "",
+    val name: String = "",
+    val age: Int = 0,
+    val description: String = "",
+    val imageId: String = "",
+    val status: String = "available",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val posterId: String = "",
+    val posterContact: String = "",
+    val category: String = "adoption"
+)
---
+++ app/src/main/java/com/yarsi/rescuepet/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.yarsi.rescuepet.utils
+
+object Constants {
+    const val APPWRITE_ENDPOINT = "https://sgp.cloud.appwrite.io/v1"
+    const val APPWRITE_PROJECT_ID = "6a254936000091ed5512"
+    const val DATABASE_ID = "rescuepet-db"
+    const val COLLECTION_ANIMALS = "animals"
+    const val BUCKET_ID = "animal-images"
+}
